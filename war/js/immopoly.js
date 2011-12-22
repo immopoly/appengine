41691c2b5f97b18c4821b26bc2ddb0172da5daf3
Stefan Hoth <sh@jnamic.com>
2011-12-22 22:28:14 +0100

    #54 Correct order of rent and provision

--- war/js/immopoly.js
+++ war/js/immopoly.js
@@ -151,8 +151,8 @@
 			entryData.push( intIndex+1 ); //Rank is index+1
 			entryData.push( user.username );
 			entryData.push( formatMoney(user.info.balance) );
-			entryData.push( formatMoney(user.info.lastProvision) );
 			entryData.push( formatMoney(user.info.lastRent) );
+			entryData.push( formatMoney(user.info.lastProvision) );
 			
 			break;
 		case "history":
